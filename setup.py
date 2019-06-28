f76d28bc1b855a7f6d0b5472de24aaf391d62ff7
Paul Bauriegel <paul.bauriegel@telekom.de>
2019-06-28 19:05:11 +0200

    Clean Setup.py from autowrap

--- setup.py
+++ setup.py
@@ -1,15 +1,10 @@
 # setup.py
-import os
-import pkg_resources
-
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
 from distutils.sysconfig import customize_compiler
 from Cython.Build.Dependencies import cythonize
 
-data_dir = pkg_resources.resource_filename("autowrap", "data_files")
-include_dir = os.path.join(data_dir, "autowrap")
 
 class my_build_ext(build_ext):
     def build_extensions(self):
@@ -28,11 +23,6 @@ class my_build_ext(build_ext):
         self.compiler.compiler_so +=["-std=c++17", "-lstdc++fs"]
         build_ext.build_extensions(self)
 
-        #https://groups.google.com/forum/#!topic/cython-users/y6y36Eiu7DE
-        #Extension("doxa", sources=["doxa/bin_cod_x.cpp"], language="c++", 
-        #            include_dirs = [include_dir, data_dir, '/usr/local/include', '/home/paul/PycharmProjects/doxa_cython/doxa', '/home/paul/PycharmProjects/doxa_cython/include'],
-        #            library_dirs=['.'],
-        #            extra_link_args=["-lm","-lc++abi", "-lstdc++fs", "-lstdc++", "-Wl,-rpath=$ORIGIN/."],) #"-Wl,--no-undefined"
 setup(
     name='doxa_cython',
     ext_modules=cythonize([
